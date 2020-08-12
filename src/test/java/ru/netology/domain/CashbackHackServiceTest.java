e0322d8009eb62537f4b966fd3ca55f7ac76bd0e
Nik <cvkeepervc@mail.ru>
2020-08-12 21:30:18 +0300

    change actual and expected

--- src/test/java/ru/netology/domain/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/domain/CashbackHackServiceTest.java
@@ -13,7 +13,7 @@ public class CashbackHackServiceTest {
         int amount = 0;
         int actual = cashbackHackService.remain(amount);
         int expected = 1000;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -21,7 +21,7 @@ public class CashbackHackServiceTest {
         int amount = 900;
         int actual = cashbackHackService.remain(amount);
         int expected = 100;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -29,7 +29,7 @@ public class CashbackHackServiceTest {
         int amount = 1000;
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -37,7 +37,7 @@ public class CashbackHackServiceTest {
         int amount = 1100;
         int actual = cashbackHackService.remain(amount);
         int expected = 900;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
 }
